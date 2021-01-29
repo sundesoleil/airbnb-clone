f69ae1322684f143b4bacbf70b963fe440090294
sundesoleil <dxrans@gmail.com>
2021-01-29 22:48:08 +0900

    users app

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (  # django authentication system
+        (
+            "Custom Profile",  # Extended profile
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    """ Custom User Model """
+
+    GENDER_FEMALE = "female"
+    GENDER_MALE = "male"
+    GENDER_OTHRE = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_FEMALE, "Female"),
+        (GENDER_MALE, "Male"),
+        (GENDER_OTHRE, "Other"),
+    )
+
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_ENGLSIH = "en"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_ENGLSIH, "English"),
+    )
+
+    CURRENCY_KRW = "krw"
+    CURRENCY_USD = "usd"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_KRW, "KRW"),
+        (CURRENCY_USD, "USD"),
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
