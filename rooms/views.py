cf82422d393a497ce449bde4e9464332e11f7d1e
sundesoleil <dxrans@gmail.com>
2021-02-12 13:40:16 +0900

    class based view

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,4 @@
-from django.views.generic import ListView
-from django.http import Http404
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -11,16 +9,12 @@ class HomeView(ListView):
     model = models.Room
     paginate_by = 10
     paginate_orphans = 5
+    ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
 
+class RoomDetail(DetailView):
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)  # DB에서 Room 정보 가져오기
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+    """RoomDetail Definition """
+
+    model = models.Room
