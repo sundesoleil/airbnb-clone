eb923b33690b539d4ae6c7a7ecc179433edd53e5
sundesoleil <dxrans@gmail.com>
2021-02-18 18:47:36 +0900

    making login Easier

--- users/views.py
+++ users/views.py
@@ -1,11 +1,26 @@
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
+class LoginView(FormView):
+
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")  # lazy : 바로 실행하지 않음
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
+
+    """def get(self, request):
         form = forms.LoginForm(initial={"email": "dxrans@gmail.com"})
         return render(request, "users/login.html", {"form": form})
 
@@ -18,7 +33,7 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
                 return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
+        return render(request, "users/login.html", {"form": form})"""
 
 
 def log_out(request):
