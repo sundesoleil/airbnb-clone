0a4f1fb10ad7dce328a518acc66f27674ee65009
sundesoleil <dxrans@gmail.com>
2021-02-19 21:38:13 +0900

    verify email

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,10 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+# Email Configuration
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "seon@sandbox5b7f1b8c7a1946ffa9f7b44588699ffc.mailgun.org"
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-from . import forms
+from . import forms, models
 
 
 class LoginView(FormView):
@@ -45,7 +45,11 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")  # lazy : 바로 실행하지 않음
-    initial = {"first_name": "Seon", "last_name": "Choi", "email": "dxrans@gmail.com"}
+    initial = {
+        "first_name": "",
+        "last_name": "",
+        "email": "",
+    }
 
     # form이 유효하다면 form.save 실행
     def form_valid(self, form):
@@ -55,4 +59,18 @@ class SignUpView(FormView):
         user = authenticate(self.request, username=email, password=password)
         if user is not None:
             login(self.request, user)
+        user.verify_email()
         return super().form_valid(form)
+
+
+def complete_verification(request, key):
+    try:
+        user = models.User.objects.get(email_secret=key)
+        user.email_verified = True
+        user.email_secret = ""
+        user.save()
+        # to do : add success message
+    except models.User.DoesNotExist:
+        # to do : add error message
+        pass
+    return redirect(reverse("core:home"))
