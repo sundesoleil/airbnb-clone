d78317cc551b2eb09f439914f5623c9cc5ee536a
sundesoleil <dxrans@gmail.com>
2021-02-23 17:29:53 +0900

    header

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        logo:'#f0454d',
+      },
+      spacing:{
+        // 왼쪽은 클래스 이름, 오른쪽은 CSS를 위한 것 
+        "25vh":"25vh", 
+        "50vh":"50vh",
+        "75vh":"75vh",  
+      },
+      borderRadius:{
+        xl: "1.5rem"
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
