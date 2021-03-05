9c016aa372c986b5df6fe54492981471e24cf9c5
sundesoleil <dxrans@gmail.com>
2021-03-05 12:46:32 +0900

    finish calendar

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 from django.urls import reverse  # url name을 필요로 하는 function
 from django_countries.fields import CountryField
@@ -121,6 +122,12 @@ class Room(core_models.TimeStampedModel):
         return photos
 
     def get_calendars(self):
-        this_month = Calendar(2021, 3)
-        next_month = Calendar(2021, 4)
-        return [this_month, next_month]
+        now = timezone.now()
+        this_year = now.year
+        this_month = now.month
+        next_month = this_month + 1
+        if this_month == 12:
+            next_month = 1
+        this_month_cal = Calendar(this_year, this_month)
+        next_month_cal = Calendar(this_year, next_month)
+        return [this_month_cal, next_month_cal]
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'sans': ['Niramit', 'Sans-serif']
+      'sans': ['Poppins', 'Sans-serif']
     },
     extend: {
       colors:{
