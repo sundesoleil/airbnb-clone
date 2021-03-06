0053fc46358c8e3a6c1870e87870cb5655c7ac41
sundesoleil <dxrans@gmail.com>
2021-03-06 13:26:50 +0900

    create reservations

--- config/urls.py
+++ config/urls.py
@@ -23,10 +23,10 @@ urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
     path("users/", include("users.urls", namespace="users")),
+    path("reservations/", include("reservations.urls", namespace="reservations")),
     path("admin/", admin.site.urls),
 ]
 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-   
