25c272d4b18facc98f10ac83c011c2909af5cde9
sundesoleil <dxrans@gmail.com>
2021-03-07 12:18:35 +0900

    cancel/confirm reservation

--- reservations/urls.py
+++ reservations/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
         views.create,
         name="create",
     ),
-    path("<int:pk>", views.ReservationDetailView.as_view(), name="detail"),
+    path("<int:pk>/", views.ReservationDetailView.as_view(), name="detail"),
+    path("<int:pk>/<str:verb>/", views.edit_reservation, name="edit"),
 ]
