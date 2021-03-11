8d193da4212365fd16d4cfa6c00c2284b52239ad
sundesoleil <dxrans@gmail.com>
2021-03-11 22:41:19 +0900

    fav list

--- lists/urls.py
+++ lists/urls.py
@@ -3,4 +3,4 @@ from . import views
 
 app_name = "lists"
 
-urlpatterns = [path("add/<int:room_pk>/", views.save_room, name="save-room")]
+urlpatterns = [path("toggle/<int:room_pk>/", views.toggle_room, name="toggle-room")]
--- lists/views.py
+++ lists/views.py
@@ -3,11 +3,15 @@ from rooms import models as room_models
 from . import models
 
 
-def save_room(request, room_pk):
+def toggle_room(request, room_pk):
+    action = request.GET.get("action", None)
     room = room_models.Room.objects.get_or_none(pk=room_pk)
-    if room is not None:
+    if room is not None and action is not None:
         the_list, _ = models.List.objects.get_or_create(
             user=request.user, name="My Favorties Houses"
         )
-        the_list.rooms.add(room)
+        if action == "add":
+            the_list.rooms.add(room)
+        elif action == "remove":
+            the_list.rooms.remove(room)
     return redirect(reverse("rooms:detail", kwargs={"pk": room_pk}))
